0391de94be8b1b94ca45c5e1211c683fe65b3554
Ervin Jay Delos Reyes <ervinjay13@live.com>
2020-06-01 12:13:11 +1200

    fourth time's the charm

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,38 +1,39 @@
-import React, { useState } from 'react';
-import fetch from 'isomorphic-unfetch'
-import ZipResponse from '../components/ZipResponse';
-import Zip from '../components/Zip';
+import React, { useState } from "react";
+import fetch from "isomorphic-unfetch";
+import ZipResponse from "../components/ZipResponse";
+import Zip from "../components/Zip";
 
 function AppContainer(props) {
+	const [responseData, setResponseData] = useState("");
 
-    const [responseData, setResponseData] = useState('');
+	const handleZipChange = async (cityValue) => {
+		//console.log(`--------- fetchData called zip:${zipValue}`)
+		const res = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${cityValue},nz`
+		);
+		const json = await res.json();
+		//console.log(json);
+		setResponseData(json);
+	};
 
-    const handleZipChange = async (zipValue) => {
-        //console.log(`--------- fetchData called zip:${zipValue}`)
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&q=${cityValue},nz`)
-        const json = await res.json()
-        //console.log(json);
-        setResponseData(json);
-    }
+	const clearResponse = () => {
+		setResponseData("");
+	};
 
-    const clearResponse = () => {
-        setResponseData('');
-    }
-
-    return (
-        <div>
-            <div className="row mt-4">
-                <div className="col-sm-4"></div>
-                <Zip onZipChange={handleZipChange} clearResponse={clearResponse}/>
-                <div className="col-sm-4"></div>
-            </div>
-            <div className="row mt-4">
-                <div className="col-sm-2"></div>
-                <ZipResponse responseData={responseData} clearResponse={clearResponse}/>
-                <div className="col-sm-2"></div>
-            </div>    
-        </div>
-    );
+	return (
+		<div>
+			<div className="row mt-4">
+				<div className="col-sm-4"></div>
+				<Zip onZipChange={handleZipChange} clearResponse={clearResponse} />
+				<div className="col-sm-4"></div>
+			</div>
+			<div className="row mt-4">
+				<div className="col-sm-2"></div>
+				<ZipResponse responseData={responseData} clearResponse={clearResponse} />
+				<div className="col-sm-2"></div>
+			</div>
+		</div>
+	);
 }
-  
-export default AppContainer
+
+export default AppContainer;
